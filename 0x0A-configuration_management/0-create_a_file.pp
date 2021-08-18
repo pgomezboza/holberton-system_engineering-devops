d6c09047f1bf3597bd1b9a56daa0d65651a9e80d
pgomezboza <pgomezboza@gmail.com>
2021-08-17 19:02:18 -0500

    add task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using Puppet to create a file with specific requeriments
+
+file { 'Create file holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
