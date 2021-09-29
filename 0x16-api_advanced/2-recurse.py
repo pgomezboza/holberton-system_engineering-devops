e2a56fea26e09719f58f06f98a09bad6a3ea0d50
pgomezboza <pgomezboza@gmail.com>
2021-09-28 23:32:53 -0500

    fix error pep8

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Recursive function that queries the Reddit API and returns a
+list containing the titles of all hot articles for a given subreddit.
+"""
+import requests
+
+url = "https://www.reddit.com/r/{}/hot.json?after={}"
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    headers = {'user-agent': 'agent'}
+    params = {'limit': after}
+    response = requests.get(
+                           url.format(subreddit, after),
+                           headers=headers,
+                           params=params,
+                           allow_redirects=False)
+
+    if response.status_code == 200:
+        key = response.json().['data']['after']
+        body = result['data']['children']
+        for n in body:
+            hot_list.append(n['data']['title'])
+        if key is not None:
+            recurse(subreddit, hot_list, key)
+        return hot_list
+    else:
+        return None
