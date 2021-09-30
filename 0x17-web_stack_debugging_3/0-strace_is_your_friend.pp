7a6e76b2d861db83c74f16c93cbe671bdfbe87db
pgomezboza <pgomezboza@gmail.com>
2021-09-29 19:36:31 -0500

    add task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix php wordpress file
 exec { 'fix-wordpress-file':
-  command => 'sed -i s/phpp/iphp/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
